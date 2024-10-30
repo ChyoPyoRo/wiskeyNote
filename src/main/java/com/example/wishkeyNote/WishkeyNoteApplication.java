82c39993d72ce02983228a6cfd79be082c7abfa7
ChyoPyoRo <allen15590@naver.com>
2024-10-30 20:30:45 +0900

    first commit

---
+++ src/main/java/com/example/wishkeyNote/WishkeyNoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.wishkeyNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishkeyNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishkeyNoteApplication.class, args);
+	}
+
+}
